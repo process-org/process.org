5ae17fa39f0168e7224cc482f29e3c92aa34cffd
processian <pray@process.org>
2022-12-01 04:46:36 +0000

    Cleanup and fix img refs

--- discept/xmlrpc0db0.php
+++ discept/xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://www.process.org/discept</homePageLink>
+		<homePageLink>https://process.org/discept</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://www.process.org/discept/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://www.process.org/discept/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://www.process.org/discept/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://www.process.org/discept/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://www.process.org/discept/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://process.org/discept/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://process.org/discept/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://process.org/discept/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://process.org/discept/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://process.org/discept/wp-json/" />
 			</apis>
 	</service>
 </rsd>
